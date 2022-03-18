6e7ddcd9a53973b02977bd2ba32a0681544185f1
hduran <hugoivandr@gmail.com>
2022-03-18 11:09:36 -0500

    First commit

---
+++ packages/pay-gateway-utility/src/components/error/NotAuth.js
@@ -0,0 +1,11 @@
+function NotAuth() {
+  return (
+    <div className="Grid Grid--alignCenter">
+      <div className="u-size6of12">
+        <span className="u-textSize5">Not - Auth</span>
+      </div>
+    </div>
+  );
+}
+
+export default NotAuth;
---
+++ packages/pay-gateway-utility/src/index.js
@@ -0,0 +1,4 @@
+export { default as GeneralError } from "./components/error/GeneralError";
+export { default as NotAuth } from "./components/error/NotAuth";
+export { default as InputField } from "./components/UI/InputField";
+export * from "./utils/get-param-url";
