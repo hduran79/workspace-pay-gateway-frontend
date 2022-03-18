e26fb9322119cd6f47ae04cc6f1a20629f02f0c3
hduran <hugoivandr@gmail.com>
2022-03-18 15:02:25 -0500

    Se borra todo el proyecto por problemas de versiones

--- packages/pay-costa-rica-frontend/src/history.js
+++
@@ -1,5 +0,0 @@
-import { useHashHistory } from "use-hash-history";
-
-const opts = { hashRoot: "/" };
-
-export default useHashHistory(opts);
--- packages/pay-gateway-utility/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as GeneralError } from "./components/error/GeneralError";
-export { default as NotAuth } from "./components/error/NotAuth";
-export { default as InputField } from "./components/UI/InputField";
-export * from "./utils/get-param-url";
